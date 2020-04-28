de77b628331872353492f87bb933302b8870938e
Arkkav <arkark@rambler.ru>
2020-04-28 15:28:57 +0300

    moved all to the root

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 
 #sudo ln -sf /home/arkkav/projects/web_project/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo nginx -s reload
+#sudo /etc/init.d/nginx restart
