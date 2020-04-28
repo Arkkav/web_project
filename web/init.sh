bb279dd8ad3f65c0bf58e00abcebbb3ba47250d9
Arkkav <arkark@rambler.ru>
2020-04-28 12:18:27 +0300

    for test

---
+++ web/init.sh
@@ -0,0 +1,4 @@
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+
+#sudo ln -sf /home/arkkav/projects/web_project/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo nginx -s reload
