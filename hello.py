b83cc362f1106e766a1cbf4c4de7e58586749db2
Arkkav <arkark@rambler.ru>
2020-04-30 12:15:45 +0300

    added gunicorn WSGI function

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    status = '200 OK'
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    response_headers = [
+        ('Content-type', 'text/plain'),
+    ]
+    start_response(status, response_headers)
+    return body
--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/arkkav/projects/web_project/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+
 #sudo nginx -s reload
 sudo /etc/init.d/nginx restart
+
+#gunicorn --config /home/arkkav/projects/web_project/etc/hello.py --reload --chdir /home/arkkav/projects/web_project hello:app
+gunicorn --config /home/box/web/etc/hello.py --reload --chdir /home/box/web hello:app
